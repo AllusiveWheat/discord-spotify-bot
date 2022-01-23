2ac05617c40e3b05e6494f6824d3e34c9b1e34da
Allusive <auraud.zarafshar@gmail.com>
2022-01-22 20:55:47 -0800

    Spotify playback

--- bot.py
+++ bot.py
@@ -1,26 +1,49 @@
 import os
-from discord import Client, Intents, Embed
-from discord_slash import SlashCommand, SlashContext
+from aiohttp import client
+from discord.ext import commands
 import lavapy
-
+bot = commands.Bot(command_prefix="!")
 from dotenv import load_dotenv
+from lavapy.ext import spotify
+
 load_dotenv()
-bot = Client(intents=Intents.default())
-slash = SlashCommand(bot)
+
 async def initialiseNodes():
     """
     Wait until the bot is ready then create a Lavapy node
     """
     await bot.wait_until_ready()
-
     await lavapy.NodePool.createNode(client=bot,
-                                     host="0.0.0.0",
+                                     host="localhost",
                                      port=2333,
-                                     password="password")
-token = os.getenv("token")
-@slash.slash(name="test")
-async def test(ctx: SlashContext):
-    embed = Embed(title="Embed Test")
-    await ctx.send(embed=embed)
+                                     password="youshallnotpass",
+                                     spotifyClient=spotify.SpotifyClient(os.getenv("clientId"), os.getenv("clientSecret")))
 
-bot.run(token)
+
+@bot.command()
+async def play(ctx: commands.Context, *query) -> None:
+    """
+    Play a Youtube song from a given search query.
+
+    If the bot is not connected, connect it to the user's voice channel. For this
+    to work, the user must be connected to a voice channel
+    """
+    if not ctx.voice_client:
+        # Bot is not connected to a voice channel
+        try:
+            player: lavapy.Player = await ctx.author.voice.channel.connect(cls=lavapy.Player)
+        except AttributeError:
+            # User is not connected to a voice channel
+            await ctx.channel.send("You must be connected to a voice channel")
+            return
+    else:
+        # Bot is connected to a voice channel
+        player: lavapy.Player = ctx.voice_client
+
+     
+    track = await spotify.SpotifyTrack.search(" ".join(query), player.node)
+    await player.play(track)
+
+
+bot.loop.create_task(initialiseNodes())
+bot.run(os.getenv("token"))
